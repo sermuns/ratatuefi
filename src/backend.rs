afff3e07eb7833de9faf7dadf9542062064ce662
Samuel Åkesson <sermuns@lysator.liu.se>
2026-06-29 18:18:51 +0200

    working PoC! slow as shit!

---
+++ src/backend.rs
@@ -0,0 +1,134 @@
+use core::fmt::Write;
+
+use ratatui::{backend::WindowSize, prelude::*};
+use thiserror::Error;
+use uefi::proto::console::text::Color as UefiColor;
+
+pub struct UefiBackend<'a> {
+    output: &'a mut uefi::proto::console::text::Output,
+}
+
+impl<'a> UefiBackend<'a> {
+    pub fn new(output: &'a mut uefi::proto::console::text::Output) -> Self {
+        Self { output }
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum UefiBackendError {}
+
+fn ratatui_fg_color_to_uefi_color(color: Color) -> UefiColor {
+    match color {
+        Color::Reset => UefiColor::White,
+        Color::Black => UefiColor::White,
+        Color::Red => UefiColor::Red,
+        Color::Green => UefiColor::Green,
+        Color::Yellow => UefiColor::Yellow,
+        Color::Blue => UefiColor::Blue,
+        Color::Magenta => UefiColor::Magenta,
+        Color::Cyan => UefiColor::Cyan,
+        // Color::Gray => UefiColor::Gray,
+        Color::DarkGray => UefiColor::DarkGray,
+        Color::LightRed => UefiColor::LightRed,
+        Color::LightGreen => UefiColor::LightGreen,
+        // Color::LightYellow => UefiColor::LightYellow,
+        Color::LightBlue => UefiColor::LightBlue,
+        Color::LightMagenta => UefiColor::LightMagenta,
+        Color::LightCyan => UefiColor::LightCyan,
+        Color::White => UefiColor::White,
+        Color::Indexed(231) => UefiColor::White,
+        Color::Indexed(252) => UefiColor::LightGray,
+        Color::Indexed(236) => UefiColor::DarkGray,
+        Color::Indexed(196) => UefiColor::Red,
+        Color::Indexed(232) => UefiColor::Blue,
+        Color::Indexed(237) => UefiColor::LightGray,
+        Color::Indexed(248) => UefiColor::DarkGray,
+        other => panic!("{:?}", other),
+    }
+}
+
+impl Backend for UefiBackend<'_> {
+    type Error = UefiBackendError;
+
+    fn flush(&mut self) -> Result<(), Self::Error> {
+        Ok(())
+    }
+
+    fn window_size(&mut self) -> Result<backend::WindowSize, Self::Error> {
+        Ok(WindowSize {
+            columns_rows: self.size().unwrap(),
+            pixels: Size::ZERO, // not necessary, I think
+        })
+    }
+
+    fn size(&self) -> Result<Size, Self::Error> {
+        let current_mode = self.output.current_mode().unwrap().unwrap();
+        Ok(Size {
+            width: current_mode.columns() as u16,
+            height: current_mode.rows() as u16,
+        })
+    }
+
+    fn clear_region(&mut self, _clear_type: backend::ClearType) -> Result<(), Self::Error> {
+        todo!()
+    }
+
+    fn clear(&mut self) -> Result<(), Self::Error> {
+        self.output.clear().unwrap();
+        Ok(())
+    }
+
+    fn set_cursor_position<P: Into<Position>>(&mut self, position: P) -> Result<(), Self::Error> {
+        let pos = position.into();
+        self.output
+            .set_cursor_position(pos.x as usize, pos.y as usize)
+            .unwrap();
+        Ok(())
+    }
+
+    fn get_cursor(&mut self) -> Result<(u16, u16), Self::Error> {
+        let (column, row) = self.output.cursor_position();
+        Ok((column as u16, row as u16))
+    }
+
+    fn get_cursor_position(&mut self) -> Result<Position, Self::Error> {
+        let (column, row) = self.output.cursor_position();
+        Ok((column as u16, row as u16).into())
+    }
+
+    fn draw<'a, I>(&mut self, content: I) -> Result<(), Self::Error>
+    where
+        I: Iterator<Item = (u16, u16, &'a buffer::Cell)>,
+    {
+        for (x, y, cell) in content {
+            self.set_cursor_position((x, y)).unwrap();
+
+            let foreground = ratatui_fg_color_to_uefi_color(cell.fg);
+            // background only allowed to be one of the first 8 colors:
+            // https://docs.rs/uefi/latest/uefi/proto/console/text/struct.Output.html#method.set_color
+            // https://docs.rs/uefi/latest/uefi/proto/console/text/enum.Color.html
+            let background = UefiColor::Brown;
+            self.output.set_color(foreground, background).unwrap();
+
+            self.output.write_str(cell.symbol()).unwrap();
+        }
+        Ok(())
+    }
+
+    fn set_cursor(&mut self, x: u16, y: u16) -> Result<(), Self::Error> {
+        self.output
+            .set_cursor_position(x as usize, y as usize)
+            .unwrap();
+        Ok(())
+    }
+
+    fn show_cursor(&mut self) -> Result<(), Self::Error> {
+        let _ = self.output.enable_cursor(true);
+        Ok(())
+    }
+
+    fn hide_cursor(&mut self) -> Result<(), Self::Error> {
+        let _ = self.output.enable_cursor(false);
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,37 @@
 #![no_std]
 
 use core::time::Duration;
+
 use log::info;
+use ratatui::{prelude::*, widgets::RatatuiMascot};
 use uefi::prelude::*;
+
+mod backend;
+
+use crate::backend::UefiBackend;
 
 #[entry]
 fn main() -> Status {
     uefi::helpers::init().unwrap();
-    info!("Hello world!");
-    boot::stall(Duration::from_secs(10));
+
+    info!("started app");
+
+    system::with_stdout(|output| {
+        output.reset(false).unwrap();
+        let backend = UefiBackend::new(output);
+        let mut terminal = Terminal::new(backend).unwrap();
+        terminal.draw(draw).unwrap();
+    });
+
+    boot::stall(Duration::MAX);
+
     Status::SUCCESS
+}
+
+fn draw(frame: &mut Frame) {
+    let area = frame.area();
+    frame.render_widget(
+        RatatuiMascot::default(),
+        area.centered(Constraint::Length(40), Constraint::Length(30)),
+    );
 }
