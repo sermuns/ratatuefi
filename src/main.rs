53711213d15011610da5ce777732d134c024520d
Samuel Åkesson <sermuns@lysator.liu.se>
2026-06-29 14:34:47 +0200

    copy simple example for x86_64-unknown-uefi

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_main]
+#![no_std]
+
+use core::time::Duration;
+use log::info;
+use uefi::prelude::*;
+
+#[entry]
+fn main() -> Status {
+    uefi::helpers::init().unwrap();
+    info!("Hello world!");
+    boot::stall(Duration::from_secs(10));
+    Status::SUCCESS
 }
