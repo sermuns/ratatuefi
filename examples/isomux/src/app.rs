139bbb2eeab696ff282137b253bd49f3a7e53d91
Samuel Åkesson <sermuns@lysator.liu.se>
2026-07-02 20:27:49 +0200

    isomux: remove unused parts

--- examples/isomux/src/app.rs
+++ examples/isomux/src/app.rs
@@ -1,7 +1,3 @@
-use core::time::Duration;
-
-use alloc::format;
-use alloc::string::String;
 use ratatuefi::UefiBackend;
 use ratatui::prelude::*;
 use uefi::prelude::*;
@@ -11,7 +7,6 @@ pub struct App<'a> {
     quit: bool,
     input: &'a mut Input,
     terminal: Terminal<UefiBackend<'a>>,
-    latest_keypress_string: String,
 }
 
 impl<'a> App<'a> {
@@ -22,13 +17,13 @@ impl<'a> App<'a> {
             quit: false,
             input,
             terminal,
-            latest_keypress_string: String::new(),
         }
     }
 
     fn handle_keystroke(&mut self) {
-        let keystroke = self.input.read_key().unwrap().unwrap();
-        self.latest_keypress_string = format!("{:?}", keystroke);
+        let Ok(Some(keystroke)) = self.input.read_key() else {
+            return;
+        };
 
         match keystroke {
             Key::Printable(c) => match c.into() {
@@ -45,20 +40,6 @@ impl<'a> App<'a> {
         while !self.quit {
             boot::wait_for_event(events_to_wait_for).unwrap();
             self.handle_keystroke();
-
-            self.terminal
-                .draw(|frame| {
-                    frame.render_widget(self.latest_keypress_string.as_str(), frame.area());
-                })
-                .unwrap();
         }
-
-        self.terminal
-            .draw(|frame| {
-                frame.render_widget("quitting!", frame.area());
-            })
-            .unwrap();
-
-        boot::stall(Duration::from_secs(5));
     }
 }
